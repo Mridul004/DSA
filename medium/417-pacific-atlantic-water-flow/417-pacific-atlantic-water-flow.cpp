eca0151cc3687be049f28fd64af6b7a71a542b5c
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-07-19 18:53:29 +0530

    Add solution for 417. Pacific Atlantic Water Flow

---
+++ medium/417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,20 @@
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+    }
+            }
+        }
+                }
+                    vis[i1][j1]=1;
+                    q.push({i1,j1});
+                if(i1>=0&&i1<n&&j1>=0&&j1<m&&heights[i1][j1]>=heights[r][c]&&vis[i1][j1]==0){
+                int j1=c+k[1];
+                int i1=r+k[0];
+            for(auto k:dir){
+            q.pop();
+            auto [r,c]=q.front();
+        while(!q.empty()){
+        vector<vector<int>> dir={{0,1},{0,-1},{1,0},{-1,0}};
+        q.push({i,j});
+        queue<pair<int,int>> q;
+        vis[i][j]=1;
+        int n=heights.size(),m=heights[0].size();
+    void bfs(int i,int j,vector<vector<int>> &vis,vector<vector<int>> heights){
