eedd5b79afa6f31ab5f08a5134407df60359d0d0
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-07-29 12:49:34 +0530

    Time: 49 ms (30.17%) | Memory: 60.6 MB (90.95%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -5,10 +5,10 @@ public:
         vector<int> last(30,0),res(n,1);
         for(int i=n-1;i>=0;i--){
             for(int k=0;k<30;k++){
-                if((nums[i]&(1<<k))>0) last[k]=i;
+                if((nums[i]&(1<<k))) last[k]=i;
                 res[i]=max(res[i],last[k]-i+1);
             }
         }
         return res;
     } 
-};
+}; 
