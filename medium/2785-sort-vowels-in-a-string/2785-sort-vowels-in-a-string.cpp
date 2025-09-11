f497138f137f457fce363ca82f60d18ae821c097
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-09-11 22:33:14 +0530

    Add solution for 2785. Sort Vowels in a String

---
+++ medium/2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,13 @@
+            }
+        }
+        sort(vowels.begin(), vowels.end());
+                vowels.push_back(c);
+            if (isVowel(c)) {
+        for (char c : s) {
+        vector<char> vowels;
+    string sortVowels(string s) {
+public:
+class Solution {
+bool isVowel(char c) {
+        return string("AEIOUaeiou").find(c) != string::npos;
+    }
