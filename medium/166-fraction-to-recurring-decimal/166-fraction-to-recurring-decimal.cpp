6a8ebe8dcbf2700487b19bcef5cdd4f7b5ddbbe0
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-09-24 20:34:11 +0530

    Add solution for 166. Fraction to Recurring Decimal

---
+++ medium/166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
@@ -0,0 +1,12 @@
+        ans+=to_string(q);
+        long long r=num%den;
+        long long q=num/den;
+        long long den=abs((long long)denominator);
+        long long num=abs((long long)numerator);
+        if ((numerator<0)^(denominator< 0)) ans+='-';
+        string ans;
+        
+        if (numerator==0) return "0";
+    string fractionToDecimal(int numerator, int denominator) {
+public:
+class Solution {
