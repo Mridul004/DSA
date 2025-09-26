d1f313de91bc5ea47749759dd9bda44ac34b2a43
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-09-26 08:30:14 +0530

    Add solution for 611. Valid Triangle Number

---
+++ medium/611-valid-triangle-number/611-valid-triangle-number.c
@@ -0,0 +1,15 @@
+            i+=f;
+            if (f==0) continue;
+            fill(nums.begin()+i, nums.begin()+i+f, x);
+            const int f=freq[x];
+        for(int i=0, x=xMin; x<=xMax; x++){
+        }
+            xMax=max(xMax, x);
+            xMin=min(xMin, x);
+            freq[x]++;
+        for(int x: nums){
+        int freq[1001]={0}, xMin=1001, xMax=0;
+    int triangleNumber(vector<int>& nums) {
+public:
+class Solution {
+        }
