fd86c474acbcb1b28a200cd43825279dc8e884e1
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-12-09 19:40:29 +0530

    Add solution for 3583. Count Special Triplets

---
+++ medium/3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,15 @@
+        int x1=nums[1], x1_2=x1<<1;
+        if (x1_2<M) Prev[x1]+=freq[x1_2];
+        freq[x1]++;
+        freq[nums[0]]++;
+public:
+    int specialTriplets(vector<int>& nums) {
+        const int n=nums.size();
+        memset(freq, 0, sizeof(freq));
+        memset(Prev, 0, sizeof(Prev));
+        long long cnt=0;
+const int M=100001;
+int freq[M]={0};
+long long Prev[M]={0};
+const int mod=1e9+7;
+class Solution {
