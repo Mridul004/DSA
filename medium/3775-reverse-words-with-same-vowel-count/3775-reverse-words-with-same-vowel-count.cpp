08a6944aa44dafd7af923e2c49f6e58147d06174
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-12-19 10:18:36 +0530

    Add solution for 3775. Reverse Words With Same Vowel Count

---
+++ medium/3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
@@ -0,0 +1,17 @@
+        }
+        if (cnt==cnt1) rev(s, l, r-1);
+            }
+                cnt+=(1<<(s[r]-'a') & vowelMask)!=0;
+            else{
+            }
+                l=r+1;
+                cnt=0;
+                else if (cnt==cnt1) rev(s, l, r-1);
+                }
+                    cnt1=cnt;
+                    find1stWord=1;
+                if (!find1stWord) {
+        
+        return s;
+    }
+};
