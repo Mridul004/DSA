5c005c4bbe951c4237bf65f333f9e712f268548c
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2026-01-03 12:10:15 +0530

    Add solution for 1411. Number of Ways to Paint N × 3 Grid

---
+++ hard/1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int mod=1000000007;
+        long long x=6,y=6;
+        for(int i=2;i<=n;i++){
+            long long x1=(3*x+2*y)%mod;
+        }
+    }
+            long long y1=(2*x+2*y)%mod;
+        return (x+y)%mod;
+            x=x1,y=y1;
+};
