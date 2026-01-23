68433af4f93b8208a1d1ad432ae5cf411d179ba3
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2026-01-24 00:12:32 +0530

    Add solution for 55. Jump Game

---
+++ medium/55-jump-game/55-jump-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int val=nums.size()-1;
+        for(int i=nums.size()-2;i>=0;i--) if(i+nums[i]>=val) val=i;
+        return val==0;
+    }
+};
